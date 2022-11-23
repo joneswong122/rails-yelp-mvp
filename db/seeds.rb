72125779fb5d437220943eab2dc4d719fa179a93
joneswong <jones_wong@icloud.com>
2022-11-23 14:48:41 +0000

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "belgian"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian"}
+epicure = {name: "Epicure", address: "75008 Paris", category: "french"}
+hungry_japanese =  {name: "Hungry Japanese", address: "High St, Southampton", category: "japanese"}
+hungry_panda =  {name: "Hungry Panda", address: "High St, Menchester", category: "chinese"}
+
+[dishoom, pizza_east, epicure, hungry_japanese, hungry_panda].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
